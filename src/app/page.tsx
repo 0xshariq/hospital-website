bbb9a73fa9601e7383b6e2a631c3a2850df6ef0f
Shariq Choudhary <khanshariq92213@gmail.com>
2024-09-24 21:04:08 +0530

    Content Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
-
+import Appointments from "./components/Appointments";
+import Doctors from "./components/Doctors";
 import Navbar from "./components/Navbar";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
-     <Navbar/>
+    <>
+      <Navbar />
+      <Services />
+      <Appointments />
+      <Doctors />
+    </>
   );
 }
