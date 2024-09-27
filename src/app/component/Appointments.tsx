4fcc7ccae1e25bcae8c129c9c445c788d11d7596
Shariq Choudhary <khanshariq92213@gmail.com>
2024-09-27 21:19:02 +0530

    Text Removed From Appointment Section

--- src/app/component/Appointments.tsx
+++ src/app/component/Appointments.tsx
@@ -2,62 +2,8 @@ import React from 'react'
 
 function Appointments() {
   return (
-    <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui. 
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris. 
-    Nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in. 
-    Reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla. 
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui.
-    Laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in.
-    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui.
+    <div>
+      Appointment Section
     </div>
   )
 }
