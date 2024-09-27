ff01e421105cfe4d5ad6af3b17b4425a7d602a0b
Shariq Choudhary <khanshariq92213@gmail.com>
2024-09-27 21:23:13 +0530

    Style Added

--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -89,7 +89,7 @@ export default function FeedbackForm() {
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
               >
                 <option value="">Select a location</option>
-                <option value="LLH Medical Center Musaffah">LLH Medical Center Musaffah</option>
+                <option value="Extra Care Medical Center" selected>Extra Care Medical Center</option>
                 {/* Add more options as needed */}
               </select>
             </div>
@@ -206,13 +206,13 @@ export default function FeedbackForm() {
             <div>
               <button
                 type="submit"
-                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 my-4"
               >
                 Submit
               </button>
               <button
                 type="reset"
-                className="flex-1 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Reset
               </button>
